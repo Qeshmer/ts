47fdbd961519ca296d216b8c374ae0e36d97ca6f
Qeshmer <talcet91@gmail.com>
2017-03-28 09:53:59 +0200

    calc

--- lab2/app/calc.js
+++ lab2/app/calc.js
@@ -1,71 +1,53 @@
-namespace calc {
-
-    class MathStuff {
-
-        private _mathExpression: string;
-
-        constructor() {
+var calc;
+(function (calc) {
+    var MathStuff = (function () {
+        function MathStuff() {
             this._mathExpression = '';
         }
-
-    };
-
-    let calcMath = new MathStuff();
-
-    let operators: Array<string> = ['+', '-', '/', '*'];
-
-    let calcScreen = $('[calc-screen]');
-    let clearButton = $('[clear]');
-    let dotButton = $('[dot]');
-    let equals = $('[equals]');
-    let backspace = $('[backspace]');
-
-    let hasNoOperator = function (): boolean {
-        let lastChar = calcScreen.val().charAt(calcScreen.val().length - 1);
+        return MathStuff;
+    }());
+    ;
+    var calcMath = new MathStuff();
+    var operators = ['+', '-', '/', '*'];
+    var calcScreen = $('[calc-screen]');
+    var clearButton = $('[clear]');
+    var dotButton = $('[dot]');
+    var equals = $('[equals]');
+    var backspace = $('[backspace]');
+    var hasNoOperator = function () {
+        var lastChar = calcScreen.val().charAt(calcScreen.val().length - 1);
         return operators.indexOf(lastChar) === -1;
     };
-
     $('[add]').on('click', function (event) {
         event.preventDefault();
-        let buttonText = $(this).attr('add');
-
+        var buttonText = $(this).attr('add');
         calcScreen.val(calcScreen.val() + buttonText);
     });
-
     $('[operator]').on('click', function (event) {
         event.preventDefault();
-        let buttonText = $(this).attr('operator');
-
-
+        var buttonText = $(this).attr('operator');
         if (calcScreen.val() !== '' && hasNoOperator()) {
             calcScreen.val(calcScreen.val() + buttonText);
         }
     });
-
     dotButton.on('click', function (event) {
         event.preventDefault();
-        let buttonText = $(this).attr('dot');
-
+        var buttonText = $(this).attr('dot');
         if (calcScreen.val().indexOf('.') === -1) {
             calcScreen.val(calcScreen.val() + buttonText);
         }
     });
-
     clearButton.on('click', function (event) {
         event.preventDefault();
-
         calcScreen.val('');
     });
-
     equals.on('click', function (event) {
         event.preventDefault();
-
         calcScreen.val(eval(calcScreen.val()));
     });
-
     backspace.on('click', function (event) {
         event.preventDefault();
-
         calcScreen.val(calcScreen.val().slice(0, calcScreen.val().length - 1));
     });
-}
+})(calc || (calc = {}));
+//# sourceMappingURL=calc.js.map
